38c9bc5d26a93bfccb0490588d65774c10bb385b
Grey Christoforo <grey@christoforo.net>
2016-06-04 16:50:04 +0100

    Create installNextcloud.sh

---
+++ installNextcloud.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+# remove info.php (prevents server info leak)
+rm /srv/http/info.php
+
+# to mount SMB shares: 
+pacman -S --noconfirm --noprogress --needed smbclient
+
+# for video file previews
+pacman -S --noconfirm --noprogress --needed ffmpeg
+
+# for document previews
+pacman -S --noconfirm --noprogress --needed libreoffice-fresh
+
+# for image previews
+pacman -S --noconfirm --noprogress --needed imagemagick ghostscript openexr openexr openexr libxml2 librsvg libpng libwebp
+
+# not 100% sure what needs this:
+pacman -S --noconfirm --noprogress --needed gamin
+
+# nextcloud itself
+su docker -c 'pacaur -Sm --noprogressbar --noedit --noconfirm nextcloud'
+pacman -U --noconfirm --needed /home/docker/.cache/pacaur/nextcloud/nextcloud-${OC_VERSION}-any.pkg.tar
+
+# setup Apache for nextcloud
+cp /etc/webapps/nextcloud/apache.example.conf /etc/httpd/conf/extra/nextcloud.conf
+sed -i 's,Alias /nextcloud "/usr/share/webapps/nextcloud",Alias /${TARGET_SUBDIR} "/usr/share/webapps/nextcloud",g' /etc/httpd/conf/extra/nextcloud.conf
+sed -i '$a Include conf/extra/nextcloud.conf' /etc/httpd/conf/httpd.conf
+
+# reduce docker layer size
+cleanup-image
